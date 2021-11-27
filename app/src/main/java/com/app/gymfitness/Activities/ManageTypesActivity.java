a2aaed014c324cefc5e665f633a1d863122ea6b3
Ghazanfar Ateeb <strange.person9125@gmail.com>
2021-11-28 03:08:32 +0500

    Database started working with add

--- app/src/main/java/com/app/gymfitness/Activities/ManageTypesActivity.java
+++ app/src/main/java/com/app/gymfitness/Activities/ManageTypesActivity.java
@@ -19,33 +19,41 @@ import java.util.List;
 
 public class ManageTypesActivity extends AppCompatActivity {
     RecyclerView recyclerview;
+    List<ClassType> classTypeList;
+    TypesAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_types);
-
         recyclerview = findViewById(R.id.recycler_view);
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
-        Cursor cursor = databaseHelper.getAllClassTypes(databaseHelper.getReadableDatabase());
 
-        List<ClassType> classTypeList = new ArrayList<>();
-        while (cursor.moveToNext()) {
-            int idColIndex = cursor.getColumnIndex(DatabaseHelper.CLASS_TYPE_ID);
-            int classTypeNameColIndex = cursor.getColumnIndex(DatabaseHelper.TYPE_NAME);
-            int classTypeDescriptionColIndex = cursor.getColumnIndex(DatabaseHelper.TYPE_DESCRIPTION);
-
-            classTypeList.add(
-              new ClassType(cursor.getInt(idColIndex), cursor.getString(classTypeNameColIndex), cursor.getString(classTypeDescriptionColIndex))
-            );
-        }
-        TypesAdapter adapter = new TypesAdapter(this);
-        adapter.setData(classTypeList);
-        recyclerview.setLayoutManager(new LinearLayoutManager(this));
-        recyclerview.setAdapter(adapter);
+        classTypeList = new ArrayList<>();
+        adapter = new TypesAdapter(this);
+        setData();
 
         findViewById(R.id.fab).setOnClickListener(view -> {
             AddClassTypeDialog addClassType =new AddClassTypeDialog();
             addClassType.show(getSupportFragmentManager(),"Add type data");
+            setData();
         });
+    }
+
+    public void setData() {
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+        Cursor cursor = databaseHelper.getAllClassTypes(databaseHelper.getReadableDatabase());
+        classTypeList.clear();
+        while (cursor.moveToNext()) {
+            int idColIndex = cursor.getColumnIndex(DatabaseHelper.TYPE_ID);
+            int classTypeNameColIndex = cursor.getColumnIndex(DatabaseHelper.TYPE_NAME);
+            int classTypeDescriptionColIndex = cursor.getColumnIndex(DatabaseHelper.TYPE_DESCRIPTION);
+
+            classTypeList.add(
+                    new ClassType(cursor.getInt(idColIndex), cursor.getString(classTypeNameColIndex), cursor.getString(classTypeDescriptionColIndex))
+            );
+        }
+
+        adapter.setData(classTypeList);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+        recyclerview.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/app/gymfitness/Dialogs/AddClassTypeDialog.java
+++ app/src/main/java/com/app/gymfitness/Dialogs/AddClassTypeDialog.java
@@ -2,16 +2,18 @@ package com.app.gymfitness.Dialogs;
 
 
 import android.app.Dialog;
-import android.content.DialogInterface;
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.app.gymfitness.DatabaseHelper.DatabaseHelper;
 import com.app.gymfitness.R;
 
 public class AddClassTypeDialog extends AppCompatDialogFragment {
@@ -21,22 +23,37 @@ public class AddClassTypeDialog extends AppCompatDialogFragment {
         AlertDialog.Builder builder= new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.add_class_type,null);
-        EditText salary = view.findViewById(R.id.et_type_name);
-        EditText name = view.findViewById(R.id.et_type_description);
+        EditText etTypeName = view.findViewById(R.id.et_type_name);
+        EditText etDescription = view.findViewById(R.id.et_type_description);
         builder.setView(view)
                 .setTitle("Add data")
-                .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                .setNegativeButton("Cancel", (dialog, which) -> {
 
-                    }
                 })
-                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                .setPositiveButton("Add", (dialog, which) -> {
+                    boolean typeNameFilled = isFieldFilled(etTypeName);
+                    boolean descriptionFilled = isFieldFilled(etDescription);
+                    if (typeNameFilled && descriptionFilled) {
+                        DatabaseHelper databaseHelper = new DatabaseHelper(getActivity().getApplicationContext());
+                        ContentValues content = new ContentValues();
+                        content.put(DatabaseHelper.TYPE_NAME,etTypeName.getText().toString());
+                        content.put(DatabaseHelper.TYPE_DESCRIPTION,etDescription.getText().toString());
+                        databaseHelper.getWritableDatabase().insert(DatabaseHelper.TYPE_TABLE_NAME, null, content);
+                        Toast.makeText(getActivity().getApplicationContext(), "Type added successfully",Toast.LENGTH_SHORT).show();
 
                     }
                 });
         return builder.create();
+    }
+
+    private boolean isFieldFilled(EditText field) {
+        boolean fieldFilled = true;
+
+        if (field.getText().toString().trim().isEmpty()) {
+            field.setError("Required field cannot be empty.");
+            fieldFilled = false;
+        }
+
+        return fieldFilled;
     }
 }
