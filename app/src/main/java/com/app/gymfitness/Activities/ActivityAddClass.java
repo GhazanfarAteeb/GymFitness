b32b18c50bda3ffdf6fb51a55c4b792219cf1cf5
awais1515 <awais.ahmadfcc@gmai.com>
2021-11-29 20:10:38 +0500

    time dialog added

--- app/src/main/java/com/app/gymfitness/Activities/ActivityAddClass.java
+++ app/src/main/java/com/app/gymfitness/Activities/ActivityAddClass.java
@@ -2,11 +2,15 @@ package com.app.gymfitness.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.app.gymfitness.R;
+
+import java.util.Calendar;
 
 public class ActivityAddClass extends AppCompatActivity {
 
@@ -19,21 +23,46 @@ public class ActivityAddClass extends AppCompatActivity {
         etEndTime = findViewById(R.id.et_end_time);
         etStartTime = findViewById(R.id.et_start_time);
         etCapacity = findViewById(R.id.et_capacity);
+        setListeners();
     }
 
     private void setListeners(){
         etEndTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(ActivityAddClass.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        etEndTime.setText( String.format("%02d" , selectedHour) + ":" + String.format("%02d" ,selectedMinute) );
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
             }
         });
 
         etStartTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(ActivityAddClass.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        etStartTime.setText( String.format("%02d" , selectedHour) + ":" + String.format("%02d" ,selectedMinute) );
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
             }
         });
+
+
     }
 }
